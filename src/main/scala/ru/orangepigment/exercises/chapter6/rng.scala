c013e63103814309dd2020d2a501fd35c15e2c1b
orangepigment <cadmium.orange@yandex.ru>
2021-10-02 12:00:58 +0300

    Chapter6 exercises in progress

---
+++ src/main/scala/ru/orangepigment/exercises/chapter6/rng.scala
@@ -0,0 +1,117 @@
+package ru.orangepigment.exercises.chapter6
+
+
+import scala.annotation.tailrec
+
+object RNG {
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
+
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
+  }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r) = rng.nextInt
+    val (d, r2) = double(r)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, r) = double(rng)
+    val (i, r2) = r.nextInt
+    ((d, i), r2)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d, r) = double(rng)
+    val (d2, r2) = double(r)
+    val (d3, r3) = double(r2)
+    ((d, d2, d3), r3)
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    @tailrec
+    def impl(count: Int)(rng: RNG)(acc: List[Int] = List.empty[Int]): (List[Int], RNG) = {
+      if (count <= 0) {
+        (acc.reverse, rng)
+      } else {
+        val (i, r) = rng.nextInt
+        impl(count - 1)(r)(i +: acc)
+      }
+    }
+
+    impl(count)(rng)()
+  }
+
+  def nonNegativeEven: Rand[Int] = RNG.map(nonNegativeInt)(i => i - i % 2)
+
+  def doubleViaMap(rng: RNG): Rand[Double] = {
+    RNG.map(nonNegativeInt)(i => i / (Int.MaxValue.toDouble + 1))
+  }
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, rng1) = ra(rng)
+      val (b, rng2) = rb(rng1)
+      (f(a, b), rng2)
+    }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+    map2(ra, rb)((_, _))
+
+  val int: Rand[Int] = _.nextInt
+
+  val randIntDouble: Rand[(Int, Double)] = both(int, double)
+
+  val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List.empty[A]))((f, acc) => map2(f, acc)(_ :: _))
+
+  def intsViaSequence(count: Int): Rand[List[Int]] = {
+    sequence(List.fill(count)(int))
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = { rng =>
+    val (i, rng2) = nonNegativeInt(rng)
+    val mod = i % n
+    if (i + (n-1) - mod >= 0)
+      (mod, rng2)
+    else nonNegativeLessThan(n)(rng)
+  }
+
+}
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
+
+object Exercises {
+
+
+
+}
